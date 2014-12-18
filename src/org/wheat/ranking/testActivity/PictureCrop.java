c5082a8e39d7fc064a4b6d56a7c61376bdb2b5d2
hogachen <hogagdufs@gmail.com>
2014-12-18 19:44:27 +0800

    fix the imageview to picture

--- src/org/wheat/ranking/testActivity/PictureCrop.java
+++ src/org/wheat/ranking/testActivity/PictureCrop.java
@@ -1,21 +1,19 @@
 package org.wheat.ranking.testActivity;
 
+
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
+
 import org.wheat.beautyranking.R;
-import org.wheat.ranking.entity.BeautyDetail;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -24,23 +22,15 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
-public class PicCutDemoActivity extends Activity implements OnClickListener {
+public class PictureCrop extends Activity implements OnClickListener {
 
+//	private ImageButton ib = null;
 	private ImageView iv = null;
-	private EditText edt_beauty_info_school=null;
-	private EditText edt_beauty_info_admission=null;
-	private EditText edt_beauty_info_birthday=null;
-	private EditText edt_beauty_info_constellation=null;
-	private EditText edt_beauty_info_description=null;
-	private Button btn_submit=null;
+//	private Button btn = null;
 	private String tp = null;
 
 	private String photoName = null;
@@ -58,14 +48,12 @@ public class PicCutDemoActivity extends Activity implements OnClickListener {
 	 * 初始化方法实现
 	 */
 	private void init() {
+//		ib = (ImageButton) findViewById(R.id.imageButton1);
 		iv = (ImageView) findViewById(R.id.imageView1);
-		edt_beauty_info_school=(EditText)findViewById(R.id.beauty_info_school);
-		edt_beauty_info_admission=(EditText)findViewById(R.id.beauty_info_admission);
-		edt_beauty_info_birthday=(EditText)findViewById(R.id.beauty_info_birthday);
-		edt_beauty_info_constellation=(EditText)findViewById(R.id.beauty_info_constellation);
-		edt_beauty_info_description=(EditText)findViewById(R.id.beauty_info_description);
-		btn_submit= (Button)findViewById(R.id.submit);
+//		btn = (Button) findViewById(R.id.button1);
+//		ib.setOnClickListener(this);
 		iv.setOnClickListener(this);
+//		btn.setOnClickListener(this);
 	}
 
 	/**
@@ -76,13 +64,16 @@ public class PicCutDemoActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-
+//		case R.id.imageButton1:
+//			ShowPickDialog();
+//			break;
 		case R.id.imageView1:
 			ShowPickDialog();
 			break;
-		case R.id.submit:
-			BeautyDetail beauty=null;
-			uploadBeautyInfo(beauty);
+//		case R.id.button1:
+//			ShowPickDialog();
+//			break;
+
 		default:
 			break;
 		}
@@ -97,8 +88,21 @@ public class PicCutDemoActivity extends Activity implements OnClickListener {
 				.setNegativeButton("相册", new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int which) {
 						dialog.dismiss();
+						/**
+						 * 刚开始，我自己也不知道ACTION_PICK是干嘛的，后来直接看Intent源码，
+						 * 可以发现里面很多东西，Intent是个很强大的东西，大家一定仔细阅读下
+						 */
 						Intent intent = new Intent(Intent.ACTION_PICK, null);
 
+						/**
+						 * 下面这句话，与其它方式写是一样的效果，如果：
+						 * intent.setData(MediaStore.Images
+						 * .Media.EXTERNAL_CONTENT_URI);
+						 * intent.setType(""image/*");设置数据类型
+						 * 如果朋友们要限制上传到服务器的图片类型时可以直接写如
+						 * ："image/jpeg 、 image/png等的类型"
+						 * 这个地方小马有个疑问，希望高手解答下：就是这个数据URI与类型为什么要分两种形式来写呀？有什么区别？
+						 */
 						intent.setDataAndType(
 								MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
 								"image/*");
@@ -109,7 +113,12 @@ public class PicCutDemoActivity extends Activity implements OnClickListener {
 				.setPositiveButton("拍照", new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int whichButton) {
 						dialog.dismiss();
-
+						/**
+						 * 下面这句还是老样子，调用快速拍照功能，至于为什么叫快速拍照，大家可以参考如下官方
+						 * 文档，you_sdk_path/docs/guide/topics/media/camera.html
+						 * 我刚看的时候因为太长就认真看，其实是错的，这个里面有用的太多了，所以大家不要认为
+						 * 官方文档太长了就不看了，其实是错的，这个地方小马也错了，必须改正
+						 */
 						Intent intent = new Intent(
 								MediaStore.ACTION_IMAGE_CAPTURE);
 
@@ -128,41 +137,72 @@ public class PicCutDemoActivity extends Activity implements OnClickListener {
 				}).show();
 	}
 
-	/**
-	 * @description 获取照片后的图片处理回调函数
-	 */
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		switch (requestCode) {
 		// 如果是直接从相册获取
 		case 1:
-			if (data != null) {
-				setPicToView(data.getData());
-			}
-
+			if (data != null)
+				startPhotoZoom(data.getData());
 			break;
 		// 如果是调用相机拍照时
 		case 2:
 
 			if (data != null && data.getData() != null) {
-				setPicToView(data.getData());
+				Uri uri = data.getData();
+				startPhotoZoom(uri);
 			}
+
+			// 一些机型无法从getData中获取uri，则需手动指定拍照后存储照片的Uri
 			if (data == null) {
 				File temp = new File(Environment.getExternalStorageDirectory()
 						+ "/" + photoName);
-				setPicToView(Uri.fromFile(temp));
+				startPhotoZoom(Uri.fromFile(temp));
 			}
 			break;
 		// 取得裁剪后的图片
 		case 3:
+			/**
+			 * 非空判断大家一定要验证，如果不验证的话， 在剪裁之后如果发现不满意，要重新裁剪，丢弃
+			 * 当前功能时，会报NullException，小马只 在这个地方加下，大家可以根据不同情况在合适的 地方做判断处理类似情况
+			 * 
+			 */
 			if (data != null) {
-				setPicToView(data.getData());
+				setPicToView(data);
 			}
 			break;
 		default:
 			break;
+
 		}
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	/**
+	 * 裁剪图片方法实现
+	 * 
+	 * @param uri
+	 */
+	public void startPhotoZoom(Uri uri) {
+		/*
+		 * 至于下面这个Intent的ACTION是怎么知道的，大家可以看下自己路径下的如下网页
+		 * yourself_sdk_path/docs/reference/android/content/Intent.html
+		 * 直接在里面Ctrl+F搜：CROP ，之前小马没仔细看过，其实安卓系统早已经有自带图片裁剪功能, 是直接调本地库的，小马不懂C C++
+		 * 这个不做详细了解去了，有轮子就用轮子，不再研究轮子是怎么 制做的了...吼吼
+		 */
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setDataAndType(uri, "image/*");
+		// 下面这个crop=true是设置在开启的Intent中设置显示的VIEW可裁剪
+		intent.putExtra("crop", "true");
+		// aspectX aspectY 是宽高的比例
+		intent.putExtra("aspectX", 3);
+		intent.putExtra("aspectY", 2);
+		// outputX outputY 是裁剪图片宽高
+		intent.putExtra("outputX", 600);
+		intent.putExtra("outputY", 400);
+		intent.putExtra("return-data", true);
+		// 将获取到的图片发给系统的intent，再做截图处理
+		startActivityForResult(intent, 3);
 	}
 
 	/**
@@ -170,18 +210,11 @@ public class PicCutDemoActivity extends Activity implements OnClickListener {
 	 * 
 	 * @param picdata
 	 */
-	private void setPicToView(Uri picdata) {
-
-		if (picdata != null) {
-			Bitmap bitmap2 = null;
-			ContentResolver contentResolver = this.getContentResolver();
-			try {
-				bitmap2 = BitmapFactory.decodeStream(contentResolver
-						.openInputStream(picdata));
-				System.out.println("image get " + bitmap2.getWidth());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+	private void setPicToView(Intent picdata) {
+		Bundle extras = picdata.getExtras();
+		if (extras != null) {
+			Bitmap photo = extras.getParcelable("data");
+			Drawable drawable = new BitmapDrawable(photo);
 
 			/**
 			 * 下面注释的方法是将裁剪之后的图片以Base64Coder的字符方式上 传到服务器，QQ头像上传采用的方法跟这个类似
@@ -199,53 +232,9 @@ public class PicCutDemoActivity extends Activity implements OnClickListener {
 			 * Bitmap dBitmap = BitmapFactory.decodeFile(tp); Drawable drawable
 			 * = new BitmapDrawable(dBitmap);
 			 */
-			setPictureAdjustToView(bitmap2);
+//			ib.setBackgroundDrawable(drawable);
+			iv.setBackgroundDrawable(drawable);
 		}
 	}
 
-	/**
-	 * 
-	* @Description: TODO 设置imageview 设配图片
-	* @author hogachen   
-	* @date 2014年12月18日 下午7:37:58 
-	* @version V1.0  
-	* @param bitmap
-	 */
-	private void setPictureAdjustToView(Bitmap bitmap) {
-		int width = getScreenWidth();
-		int picWidth = bitmap.getWidth();
-		int picHeight = bitmap.getHeight();
-		int height = (int) (width * 1.0 / picWidth * picHeight);
-		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width,
-				height);
-		iv.setLayoutParams(params);
-		iv.setImageBitmap(bitmap);
-	}
-
-	/**
-	 * 
-	* @Description: 获取屏幕宽度
-	* @author hogachen   
-	* @date 2014年12月18日 下午7:39:14 
-	* @version V1.0  
-	* @return
-	 */
-	private int getScreenWidth() {
-		WindowManager wm = this.getWindowManager();
-		int width = wm.getDefaultDisplay().getWidth();
-		return width;
-	}
-
-	/**
-	 * 
-	* @Description: 将新创建beauty 上传到服务器
-	* @author hogachen   
-	* @date 2014年12月18日 下午7:40:13 
-	* @version V1.0  
-	* @param beautyInfo
-	* @return
-	 */
-	private int uploadBeautyInfo(BeautyDetail beautyInfo){
-		return 1;
-	}
 }
